fe1bd8a7b15f7fd84d61a071525cf8045dc7a86d
Zlatin Zlatev <z.zlatev88@gmail.com>
2022-08-28 18:51:52 +0300

    Filter by Age solution

---
+++ src/FunctionalProgramming/Exercise/_6_CustomMinFunction.java
@@ -0,0 +1,27 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class _6_CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int [] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Function<int [], Integer> function =  (int[] arr) -> {
+
+            int min = Integer.MAX_VALUE;
+
+            for (int element : arr) {
+
+                if (element<min) {
+                    min = element;
+                }
+            }
+            return min;
+        };
+        System.out.println(function.apply(numbers));
+    }
+}
